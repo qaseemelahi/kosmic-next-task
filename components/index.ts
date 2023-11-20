e6e51c46224e765f6e7747b85e95fa3871568407
qaseem <qaseemelahi@gmail.com>
2023-11-20 23:09:15 +0500

    fixed file name

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,4 @@
 export { default as Text } from "./Text";
 export { default as Header } from "./Header";
-export { default as Dropdown } from "./Dropdown";
+export { default as Dropdown } from "./AutoCompleteDropdown";
 export { default as Slider } from "./Slider";
